9cbebf532beb2151417e481987e98590e2df8c95
starovoitov anton <starovoitov.anton@gmail.com>
2019-03-25 12:08:42 +0500

    get_groups func added

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+APP_TOKEN = os.getenv("APP_TOKEN")
+print(APP_TOKEN)
 
 
 def download_image(url, name, extension="jpg"):
@@ -13,11 +22,28 @@ def get_comix_json(comix_number):
     print(comix_json)
     return comix_json
 
+
 def main():
     comix_number = "614"
     # download_image("https://imgs.xkcd.com/comics/python.png")
     comix_json = get_comix_json(comix_number)
     download_image(comix_json["img"], comix_number)
+    comix_comment = comix_json["alt"]
+    print(comix_comment)
+    groups_json = get_groups_json()
+    print(groups_json)
+
+
+def get_groups_json():
+    params = {
+        "access_token": APP_TOKEN,
+        "v": "5.92",
+    }
+    # url = f"https://api.vk.com/method/groups.get?access_token={APP_TOKEN}&v=5.92"
+    url = f"https://api.vk.com/method/groups.get"
+    response = requests.get(url, params=params)
+    return response.json()
+
 
 if __name__ == '__main__':
     main()
