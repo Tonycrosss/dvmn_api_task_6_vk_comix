52deedb5344dc50aebfc4ec8b63cbbc89b65ac36
starovoitov anton <starovoitov.anton@gmail.com>
2019-03-25 15:43:23 +0500

    get_wall, upload_image, post_photo funcs added

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import requests
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 APP_TOKEN = os.getenv("APP_TOKEN")
-print(APP_TOKEN)
+GROUP_ID = "179323429"
 
 
 def download_image(url, name, extension="jpg"):
@@ -19,19 +18,7 @@ def download_image(url, name, extension="jpg"):
 def get_comix_json(comix_number):
     response = requests.get(f"https://xkcd.com/{comix_number}/info.0.json")
     comix_json = response.json()
-    print(comix_json)
     return comix_json
-
-
-def main():
-    comix_number = "614"
-    # download_image("https://imgs.xkcd.com/comics/python.png")
-    comix_json = get_comix_json(comix_number)
-    download_image(comix_json["img"], comix_number)
-    comix_comment = comix_json["alt"]
-    print(comix_comment)
-    groups_json = get_groups_json()
-    print(groups_json)
 
 
 def get_groups_json():
@@ -39,10 +26,65 @@ def get_groups_json():
         "access_token": APP_TOKEN,
         "v": "5.92",
     }
-    # url = f"https://api.vk.com/method/groups.get?access_token={APP_TOKEN}&v=5.92"
     url = f"https://api.vk.com/method/groups.get"
     response = requests.get(url, params=params)
     return response.json()
+
+
+def get_wall_upload_server_json():
+    params = {
+        "access_token": APP_TOKEN,
+        "v": "5.92",
+        "group_id": GROUP_ID
+    }
+    url = f"https://api.vk.com/method/photos.getWallUploadServer"
+    response = requests.post(url, params=params)
+    return response.json()
+
+
+def upload_image_resp_json(upload_url):
+    img = open('./614.jpg', 'rb')
+    params = {'photo': img}
+    url = upload_url
+    response = requests.post(url, files=params).json()
+    params = {
+        "access_token": APP_TOKEN,
+        "v": "5.92",
+        "group_id": GROUP_ID,
+        "photo": response["photo"],
+        "hash": response["hash"],
+        "server": response["server"]
+    }
+    url = f"https://api.vk.com/method/photos.saveWallPhoto"
+    response = requests.post(url, params=params)
+    return response.json()
+
+
+def post_photo(comix_comment, media_id, owner_id):
+    photo_id = f"photo{owner_id}_{media_id}"
+    params = {
+        "access_token": APP_TOKEN,
+        "v": "5.92",
+        "owner_id": f"-{GROUP_ID}",
+        "message": comix_comment,
+        "attachments": photo_id,
+    }
+    url = f"https://api.vk.com/method/wall.post"
+    response = requests.post(url, params=params)
+    return response.json()
+
+
+def main():
+    comix_number = "614"
+    comix_json = get_comix_json(comix_number)
+    download_image(comix_json["img"], comix_number)
+    comix_comment = comix_json["alt"]
+    upload_server_json = get_wall_upload_server_json()
+    upload_server = upload_server_json['response']['upload_url']
+    uploaded_image_resp_json = upload_image_resp_json(upload_server)
+    media_id = uploaded_image_resp_json["response"][0]["id"]
+    owner_id = uploaded_image_resp_json["response"][0]["owner_id"]
+    post_photo(comix_comment, media_id, owner_id)
 
 
 if __name__ == '__main__':
