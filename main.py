b4e408a9116dcef3329ac1061547a81e2e57b531
starovoitov anton <starovoitov.anton@gmail.com>
2019-03-29 09:41:56 +0500

    resolved dvmn's notes

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import random
 from dotenv import load_dotenv
 
 # если убрать эту функцию в main, то тогда не находит переменные ниже, вездe None
-load_dotenv()
 
-CLIENT_ID = os.getenv("CLIENT_ID")
-APP_TOKEN = os.getenv("APP_TOKEN")
+
+# CLIENT_ID = os.getenv("CLIENT_ID")
+
 GROUP_ID = "179323429"
 
 
@@ -17,15 +17,20 @@ def download_image(url, name, extension="jpg"):
         f.write(response.content)
 
 
-def get_comix_json(comix_number):
+def get_comix(comix_number):
     response = requests.get(f"https://xkcd.com/{comix_number}/info.0.json")
     comix_json = response.json()
     return comix_json
 
+def get_comix_count():
+    response = requests.get(f"https://xkcd.com/info.0.json").json()
+    comix_count = response["num"]
+    return comix_count
 
-def get_groups_json():
+
+def get_groups(app_token):
     params = {
-        "access_token": APP_TOKEN,
+        "access_token": app_token,
         "v": "5.92",
     }
     url = f"https://api.vk.com/method/groups.get"
@@ -33,9 +38,9 @@ def get_groups_json():
     return response.json()
 
 
-def get_wall_upload_server_json():
+def get_wall_upload_server(app_token):
     params = {
-        "access_token": APP_TOKEN,
+        "access_token": app_token,
         "v": "5.92",
         "group_id": GROUP_ID
     }
@@ -44,13 +49,13 @@ def get_wall_upload_server_json():
     return response.json()
 
 
-def upload_image_resp_json(upload_url, comix_number):
+def upload_image_resp(upload_url, comix_number, app_token):
     with open(f'./{comix_number}.jpg', 'rb') as img:
         params = {'photo': img}
         url = upload_url
         response = requests.post(url, files=params).json()
         params = {
-            "access_token": APP_TOKEN,
+            "access_token": app_token,
             "v": "5.92",
             "group_id": GROUP_ID,
             "photo": response["photo"],
@@ -62,10 +67,10 @@ def upload_image_resp_json(upload_url, comix_number):
     return response.json()
 
 
-def post_photo(comix_comment, media_id, owner_id):
+def post_photo(comix_comment, media_id, owner_id, app_token):
     photo_id = f"photo{owner_id}_{media_id}"
     params = {
-        "access_token": APP_TOKEN,
+        "access_token": app_token,
         "v": "5.92",
         "owner_id": f"-{GROUP_ID}",
         "message": comix_comment,
@@ -77,16 +82,19 @@ def post_photo(comix_comment, media_id, owner_id):
 
 
 def main():
-    comix_number = str(random.randint(1, 1000))
-    comix_json = get_comix_json(comix_number)
-    download_image(comix_json["img"], comix_number)
+    load_dotenv()
+    app_token = os.getenv("APP_TOKEN")
+    comix_count = int(get_comix_count())
+    comix_random_number = str(random.randint(1, comix_count))
+    comix_json = get_comix(comix_random_number)
+    download_image(comix_json["img"], comix_random_number)
     comix_comment = comix_json["alt"]
-    upload_server_json = get_wall_upload_server_json()
+    upload_server_json = get_wall_upload_server(app_token)
     upload_server = upload_server_json['response']['upload_url']
-    uploaded_image_resp_json = upload_image_resp_json(upload_server, comix_number)
+    uploaded_image_resp_json = upload_image_resp(upload_server, comix_random_number, app_token)
     media_id = uploaded_image_resp_json["response"][0]["id"]
     owner_id = uploaded_image_resp_json["response"][0]["owner_id"]
-    post_photo(comix_comment, media_id, owner_id)
+    post_photo(comix_comment, media_id, owner_id, app_token)
 
 
 if __name__ == '__main__':
